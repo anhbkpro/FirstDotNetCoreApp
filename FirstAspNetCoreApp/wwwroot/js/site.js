95f379ba7093574cb9a2a0328486e77723108c51
lai.anh <lai.anh@outlook.com>
2017-06-11 15:10:26 +0700

    -Using jQuery to build toggle menu

--- FirstAspNetCoreApp/wwwroot/js/site.js
+++ FirstAspNetCoreApp/wwwroot/js/site.js
@@ -1,22 +1,33 @@
 ﻿// site.js
 (function () {
-    var name = $("#userName");
-    name.text("Francis"); // text(1)
+    //var name = $("#userName");
+    //name.text("Francis"); // text(1)
 
-    var main = $("#main");
+    //var main = $("#main");
 
-    main.on("mouseenter", function () {
-        main.style = "background-color: #888;";
-    });
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
 
-    main.on("mouseleave", function () {
-        main.style = "";
-    });
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
 
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () { // it's magical in here, you can wire the function with each item in menuItems when it is clicked
-        var me = $(this);
-        alert(me.text()); // Note: text(2) when you pass a parameter into text() function --> it sets the text, against it returns the text that is contained in that element
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () { // it's magical in here, you can wire the function with each item in menuItems when it is clicked
+    //    var me = $(this);
+    //    alert(me.text()); // Note: text(2) when you pass a parameter into text() function --> it sets the text, against it returns the text that is contained in that element
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper"); // I add $ to $sidebarAndWrapper --> I can more easily as I read it, know that this is a jQuery object
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar"); // hide-sidebar: name of class we will toggle
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
 
 })();
