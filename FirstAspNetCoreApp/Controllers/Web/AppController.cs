ed3aac3e1981c7a086ee88bc80f8def89debb8c5
lai.anh <lai.anh@outlook.com>
2017-06-12 06:26:47 +0700

    MVC6: create Layout, TagHelpers, Validation, Service

---
+++ FirstAspNetCoreApp/Controllers/Web/AppController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstAspNetCoreApp.ViewModels;
+using FirstAspNetCoreApp.Services;
+
+namespace FirstAspNetCoreApp.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- FirstAspNetCoreApp/Startup.cs
+++ FirstAspNetCoreApp/Startup.cs
@@ -11,15 +11,36 @@ namespace FirstAspNetCoreApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             // the order of middlewares are important
-            app.UseDefaultFiles();
+            //app.UseDefaultFiles();
+
+            // not good
+            //#if DEBUG
+            //            app.UseDeveloperExceptionPage();
+            //#endif
+
+            // this is better
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new {controller = "App", action = "Index"}
+                    );
+            });
             //loggerFactory.AddConsole();
 
             //if (env.IsDevelopment())
